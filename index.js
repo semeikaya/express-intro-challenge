53b5470cdfd318499f43a92859bcfbe4d2e39ecf
Bers <semeikayaskaevih@gmail.com>
2022-09-07 09:43:44 +0300

    завершил Знакомство с Express

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3005
+const port = 3000
 
-app.get('/us', (req, res) => {
-    res.send('Hello ghbvtdfgfgkljdkfg')
+app.get('/', (req, res) => {
+    res.send('Поихали!')
 })
+app.get('/users', (req, res) => {
+    res.json(["user1", "user2", "user3"])
+})
+app.post('/users', (req, res) => {
+    res.send("юзер добавлен")
+})
+app.delete('/users/:id', (req, res) => {
+    res.send("юзер с ID :id удален")
+})
+app.patch('/users/:id', (req, res) => {
+    res.send("юзер с ID :id изменен")
+})
+app.get('/users/admin', (req, res) => {
+    res.send("не хватает прав доступа")
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
